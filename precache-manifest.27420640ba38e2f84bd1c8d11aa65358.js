5a4922f33fd2ae3f9ac11c34e21b358ded6a4907
Aaronivanamaro <aaronivanamaro@gmail.com>
2021-05-18 00:42:25 -0300

    Updates

---
+++ precache-manifest.27420640ba38e2f84bd1c8d11aa65358.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5865b4747b395dad29d2f0dc6f82766c",
+    "url": "/portfolio.ai/index.html"
+  },
+  {
+    "revision": "226d3d47a074bd512cbc",
+    "url": "/portfolio.ai/static/css/3.fd4d520b.chunk.css"
+  },
+  {
+    "revision": "08002c7d6265c8e0c2b0",
+    "url": "/portfolio.ai/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "9c488e48efa06cb28315",
+    "url": "/portfolio.ai/static/css/main.9360e1c7.chunk.css"
+  },
+  {
+    "revision": "ad45748fa46ff922c7a8",
+    "url": "/portfolio.ai/static/js/2.2fd87a26.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio.ai/static/js/2.2fd87a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226d3d47a074bd512cbc",
+    "url": "/portfolio.ai/static/js/3.aa18aff1.chunk.js"
+  },
+  {
+    "revision": "08002c7d6265c8e0c2b0",
+    "url": "/portfolio.ai/static/js/4.8e7f0fda.chunk.js"
+  },
+  {
+    "revision": "9c488e48efa06cb28315",
+    "url": "/portfolio.ai/static/js/main.26dc5d39.chunk.js"
+  },
+  {
+    "revision": "9a1d92774665c5cba793",
+    "url": "/portfolio.ai/static/js/runtime-main.186e2827.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio.ai/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7cd04809d16368af81fbef589478c27d",
+    "url": "/portfolio.ai/static/media/Granaderos.7cd04809.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio.ai/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio.ai/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio.ai/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio.ai/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio.ai/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio.ai/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "709a0fc16ee4f1077041094e56507a09",
+    "url": "/portfolio.ai/static/media/personalBudget.709a0fc1.png"
+  },
+  {
+    "revision": "f9e3c9817daa844f1ac71bf84c9c642f",
+    "url": "/portfolio.ai/static/media/pianoApp.f9e3c981.png"
+  },
+  {
+    "revision": "2b48a3b49405fd71b2d2b054860c64dc",
+    "url": "/portfolio.ai/static/media/privateTeacher.2b48a3b4.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio.ai/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "7effd061a815b979deaaf31c4b7dfddf",
+    "url": "/portfolio.ai/static/media/selfTaught.7effd061.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio.ai/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "240ab298735802d78cc6a75c5014b462",
+    "url": "/portfolio.ai/static/media/softSkills.240ab298.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/portfolio.ai/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "1cd7d91c22b1f4380f6cab628ef5fd26",
+    "url": "/portfolio.ai/static/media/techSkills.1cd7d91c.png"
+  },
+  {
+    "revision": "af9358f325720ec178ea7592dcab179c",
+    "url": "/portfolio.ai/static/media/theRestaurant.af9358f3.png"
+  },
+  {
+    "revision": "6771f8c2a7075f6ef71baad01f365798",
+    "url": "/portfolio.ai/static/media/todoApp.6771f8c2.png"
+  },
+  {
+    "revision": "bc1cb059f472f33442e1fe8645c76975",
+    "url": "/portfolio.ai/static/media/watchApp.bc1cb059.png"
+  },
+  {
+    "revision": "05f5d40894156b76e02608832ef899db",
+    "url": "/portfolio.ai/static/media/weatherApp.05f5d408.png"
+  },
+  {
+    "revision": "ec16c50de6b381b0d19b8b2fb49844db",
+    "url": "/portfolio.ai/static/media/webDeveloper.ec16c50d.png"
+  }
+]);
